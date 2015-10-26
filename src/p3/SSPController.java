1e726091140d7af293dfe4bd6f973ce49407158e
sebhero <trevligt.att.ses@gmail.com>
2015-10-26 13:58:52 +0100

    kommenterat p3

--- src/p3/SSPController.java
+++ src/p3/SSPController.java
@@ -12,138 +12,138 @@ import java.text.MessageFormat;
  */
 public class SSPController {
 
-    private SSPViewer viewer;
-    private SSPPlayer cpuPlayer;
-    private int cpuWon= 0;
-    private int playerWon = 0;
-    private String winner;
-    private boolean playingStatus =false;
-    private SSPUserInput userInput;
+	private SSPViewer viewer;
+	private SSPPlayer cpuPlayer;
+	private int cpuWon = 0;
+	private int playerWon = 0;
+	private String winner;
+	private boolean playingStatus = false;
+	private SSPUserInput userInput;
 
+	/**
+	 * Controller for the SSP game
+	 * 
+	 * @param player
+	 *            CPU player
+	 * @param viewer
+	 *            the view to display game results
+	 */
+	public SSPController(SSPPlayer player, SSPViewer viewer) {
+		this.cpuPlayer = player;
+		this.viewer = viewer;
+	}
 
-    /**
-     * Controller for the SSP game
-     * @param player CPU player
-     * @param viewer the view to display game results
-     */
-    public SSPController(SSPPlayer player, SSPViewer viewer) {
-        this.cpuPlayer = player;
-        this.viewer = viewer;
-    }
+	/**
+	 * Handles starting a new game
+	 */
+	public void newGame() {
+		cpuWon = 0;
+		playerWon = 0;
+		playingStatus = true;
+		this.userInput.setGameStatus(playingStatus);
+		updateView(4, 4);
 
-    /**
-     * Handles starting a new game
-     */
-    public void newGame() {
-        cpuWon = 0;
-        playerWon =0;
-        playingStatus = true;
-        this.userInput.setGameStatus(playingStatus);
-        updateView(4, 4);
+	}
 
-    }
+	/**
+	 * Handle finished game
+	 */
+	public void doGameEnd() {
 
-    /**
-     * Handle finished game
-     */
-    public void doGameEnd() {
+		// show text how is winner
+		System.out.println(MessageFormat.format("Winner is {0}", winner));
+		this.viewer.showWinner(MessageFormat.format("Winner is {0}", winner));
+		// Tells the inputs model that the game has ended
+		playingStatus = false;
+		this.userInput.setGameStatus(playingStatus);
+	}
 
+	/**
+	 * Handle when player makes a new choice
+	 * 
+	 * @param playerChoice
+	 *            the new choice from player
+	 */
+	public void newChoice(int playerChoice) {
 
-        //show text how is winner
-        System.out.println(MessageFormat.format("Winner is {0}",winner));
-        this.viewer.showWinner(MessageFormat.format("Winner is {0}",winner));
-        //Tells the inputs model that the game has ended
-        playingStatus = false;
-        this.userInput.setGameStatus(playingStatus);
-    }
+		int cpuChoice = cpuPlayer.newChoice();
+		System.out.println(MessageFormat.format("player choose: {0} and cpu choose> {1}", playerChoice, cpuChoice));
 
-    /**
-     * Handle when player makes a new choice
-     * @param playerChoice the new choice from player
-     */
-    public void newChoice(int playerChoice) {
+		// 0 lika
+		// -1 och 2 human vinn
+		// otherwise cpu win
 
-        int cpuChoice = cpuPlayer.newChoice();
-        System.out.println(MessageFormat.format("player choose: {0} and cpu choose> {1}",playerChoice,cpuChoice));
+		int result = playerChoice - cpuChoice;
 
-        // 0 lika
-        // -1 och 2 human vinn
-        // otherwise cpu win
+		switch (result) {
 
+		case 0:
+			System.out.println("Same, no winner");
+			break;
+		case -1:
+		case 2:
+			System.out.println("Player won round");
+			playerWon++;
+			break;
+		default:
+			System.out.println("CPU won round");
+			cpuWon++;
+			break;
+		}
 
-        int result = playerChoice - cpuChoice;
+		// Update the view
+		updateView(playerChoice, cpuChoice);
 
-        switch (result) {
+		if (checkForWinner(playerWon, cpuWon))
+		{
+			doGameEnd();
+		}
 
-            case 0:
-                System.out.println("Same, no winner");
-                break;
-            case -1:
-            case 2:
-                System.out.println("Player won round");
-                playerWon++;
-                break;
-            default:
-                System.out.println("CPU won round");
-                cpuWon++;
-                break;
-        }
+	}
 
+	/**
+	 * Updates the view with new game stats
+	 * 
+	 * @param playerChoice
+	 * @param cpuChoice
+	 */
+	private void updateView(int playerChoice, int cpuChoice) {
 
-        //Update the view
-        updateView(playerChoice, cpuChoice);
+		this.viewer.updateWithNewStats(this.playerWon, this.cpuWon, playerChoice, cpuChoice);
+	}
 
-        if(checkForWinner(playerWon,cpuWon))
-        {
-            doGameEnd();
-        }
+	/**
+	 * Check if there is a winner
+	 * 
+	 * @param playerWon
+	 *            times player has won
+	 * @param cpuWon
+	 *            times cpu has won
+	 * @return true if there is a winner
+	 */
+	private boolean checkForWinner(int playerWon, int cpuWon) {
 
+		if (playerWon == 3)
+		{
+			winner = "Player";
+			return true;
+		}
+		if (cpuWon == 3) {
+			winner = "CPU";
+			return true;
+		}
+		// no Winner
+		return false;
 
+	}
 
-
-    }
-
-    /**
-     * Updates the view with new game stats
-     * @param playerChoice
-     * @param cpuChoice
-     */
-    private void updateView(int playerChoice, int cpuChoice) {
-
-
-        //TODO add display in view values.
-        this.viewer.updateWithNewStats(this.playerWon,this.cpuWon,playerChoice,cpuChoice);
-    }
-
-    /**
-     * Check if there is a winner
-     * @param playerWon times player has won
-     * @param cpuWon times cpu has won
-     * @return true if there is a winner
-     */
-    private boolean checkForWinner(int playerWon, int cpuWon) {
-
-        if (playerWon == 3)
-        {
-            winner = "Player";
-            return true;
-        }
-        if (cpuWon == 3) {
-            winner = "CPU";
-            return true;
-        }
-        //no Winner
-        return false;
-
-
-    }
-
-    /**
-     * Set the userInputs to controller. Used for handeling new game and end game
-     * @param sspUserInput the user input object
-     */
-    public void setUserInput(SSPUserInput sspUserInput) {
-        this.userInput = sspUserInput;
-    }
+	/**
+	 * Set the userInputs to controller. Used for handeling new game and end game
+	 * 
+	 * @param sspUserInput
+	 *            the user input object
+	 */
+	public void setUserInput(SSPUserInput sspUserInput) {
+		this.userInput = sspUserInput;
+	}
 }
-
