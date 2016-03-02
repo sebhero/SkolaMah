9667879020046bec176888b910c402f5372c3b07
seb <trevligt.att.ses@gmail.com>
2016-03-02 17:26:13 +0100

    lab14 vanliga upg klara

---
+++ src/DA353A_programmering2_datastrukturer/labbar/lab14/Room.java
@@ -0,0 +1,43 @@
+/*
+ * Author: Sebastian Börebäck
+ * Copyright (c) 2016.
+ */
+
+package DA353A_programmering2_datastrukturer.labbar.lab14;
+
+public class Room {
+    private int weight;
+    private boolean left;
+    private boolean up;
+    private boolean right;
+    private boolean down;
+
+    public Room(int weight, boolean left, boolean up, boolean right,
+            boolean down) {
+        this.weight = weight;
+        this.left = left;
+        this.up = up;
+        this.right = right;
+        this.down = down;
+    }
+
+    public boolean getDown() {
+        return down;
+    }
+
+    public boolean getLeft() {
+        return left;
+    }
+
+    public boolean getRight() {
+        return right;
+    }
+
+    public boolean getUp() {
+        return up;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
